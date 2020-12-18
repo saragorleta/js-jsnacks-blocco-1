870f5e8b55d120b4fa9a50100804492876cc2891
Sara <saragorleta@yahoo.com>
2020-12-18 15:42:03 +0100

    add primo snack

---
+++ primo-snack/js/script.js
@@ -0,0 +1,20 @@
+//Primo snack
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+//Esegui questo programma in due versioni, con il for e con il while
+
+// var somma=0;
+// for(var i=0; i<5; i++){
+//   var numero=parseInt(prompt('inserisci un numero'));
+//   somma=somma+numero;
+// }
+// console.log('la somma totale dei numeri è ' somma);
+
+
+var=i;
+while(i<5){
+  var numero=parseInt(prompt('inserisci un numero'));
+  i++;
+  somma=somma+numero;
+}
+console.log('la somma totale dei numeri è ' somma);
